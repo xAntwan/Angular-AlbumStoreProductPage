8afe65c8be158c8c02ca30a6e7a6a4f35fb2e134
xAntwan <52940191+xAntwan@users.noreply.github.com>
2020-06-11 15:45:31 -0400

    Changed Number to number

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -8,7 +8,7 @@ export class ProductService {
   constructor(private _http: Http) { }
   private _albumUrl: string = '../assets/album.json';
 
-  getAlbum(id: Number){
+  getAlbum(id: number){
     
     return this._http.get(this._albumUrl).map((response) => response.json());
   }
